217b50d6c808699a91ea9c5c095e75d4b218477a
Samuel Alev <samuel.alev@frontify.com>
2025-11-30 21:06:32 +0100

    chore: update deps

--- src/frontify.ts
+++ src/frontify.ts
@@ -1,41 +1,41 @@
-import { spawn } from 'node:child_process';
-import { info } from '@actions/core';
-import { globSync } from 'glob';
+import { spawn } from "node:child_process";
+import { info } from "@actions/core";
+import { globSync } from "glob";
 
 export const deploy = async (token: string, instanceDomain: string, pathGlob: string, extraArgs: string): Promise<void> => {
-    const appPaths = globSync(pathGlob);
-    info(`Found ${appPaths.length} apps for path "${pathGlob}":`);
-    info(`- ${appPaths.join('\n- ')}`);
+	const appPaths = globSync(pathGlob);
+	info(`Found ${appPaths.length} apps for path "${pathGlob}":`);
+	info(`- ${appPaths.join("\n- ")}`);
 
-    if (appPaths.length === 0) {
-        throw new Error(`No apps found for path "${pathGlob}"!`);
-    }
+	if (appPaths.length === 0) {
+		throw new Error(`No apps found for path "${pathGlob}"!`);
+	}
 
-    for (const appPath of appPaths) {
-        info(`Start deploying "${appPath}" to ${instanceDomain}...`);
-        await deployApp(appPath, `--token ${token} --instance ${instanceDomain}`, extraArgs);
-        info(`Deployed "${appPath}"!`);
-    }
+	for (const appPath of appPaths) {
+		info(`Start deploying "${appPath}" to ${instanceDomain}...`);
+		await deployApp(appPath, `--token ${token} --instance ${instanceDomain}`, extraArgs);
+		info(`Deployed "${appPath}"!`);
+	}
 };
 
 const deployApp = async (path: string, ...extraArgs: string[]): Promise<void> => {
-    return new Promise((resolve, reject) => {
-        const childProcess = spawn(`cd ${path} && npx frontify-cli deploy ${extraArgs.join(' ')}`, [], { shell: true });
+	return new Promise((resolve, reject) => {
+		const childProcess = spawn(`cd ${path} && npx frontify-cli deploy ${extraArgs.join(" ")}`, [], { shell: true });
 
-        childProcess.stdout.on('data', (data) => {
-            info(data.toString());
-        });
+		childProcess.stdout.on("data", (data) => {
+			info(data.toString());
+		});
 
-        childProcess.stderr.on('data', (data) => {
-            info(data.toString());
-        });
+		childProcess.stderr.on("data", (data) => {
+			info(data.toString());
+		});
 
-        childProcess.on('exit', (code) => {
-            if (code === 0) {
-                resolve();
-            } else {
-                reject(`Deployment failed for "${path}" with code ${code}`);
-            }
-        });
-    });
+		childProcess.on("exit", (code) => {
+			if (code === 0) {
+				resolve();
+			} else {
+				reject(`Deployment failed for "${path}" with code ${code}`);
+			}
+		});
+	});
 };
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { getInput, setFailed } from '@actions/core';
+import { getInput, setFailed } from "@actions/core";
 
-import { deploy } from './frontify';
+import { deploy } from "./frontify";
 
 async function run(): Promise<void> {
-    try {
-        const token = getInput('token');
-        const instanceDomain = getInput('instanceDomain');
-        const extraArgs = getInput('args');
-        const path = getInput('path');
+	try {
+		const token = getInput("token");
+		const instanceDomain = getInput("instanceDomain");
+		const extraArgs = getInput("args");
+		const path = getInput("path");
 
-        await deploy(token, instanceDomain, path, extraArgs);
-    } catch (error) {
-        setFailed(error as Error | string);
-    }
+		await deploy(token, instanceDomain, path, extraArgs);
+	} catch (error) {
+		setFailed(error as Error | string);
+	}
 }
 
 run();
